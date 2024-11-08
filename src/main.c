75eade888a543536c5e5aca485fca1f2cfd44fe3
Mitja <123660565+MitjaCH@users.noreply.github.com>
2024-11-08 12:42:39 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "csv_parser.h"
+#include "json_writer.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <input.csv> <output.json>\n", argv[0]);
+        return 1;
+    }
+
+    const char *csv_file = argv[1];
+    const char *json_file = argv[2];
+
+    if (convert_csv_to_json(csv_file, json_file) == 0) {
+        printf("Conversion successful: %s\n", json_file);
+    } else {
+        printf("Conversion failed.\n")
+    }
+
+    return 0;
+}
